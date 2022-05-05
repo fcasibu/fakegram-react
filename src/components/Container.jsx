b4d4609ddca7d93f9e775a16e3b179a5a7f5e64d
nevz <projectnevz@gmail.com>
2022-05-05 22:17:53 +0800

    Add style for Container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Container(props) {
-  return <div {...props}>{props.children}</div>;
+  return (
+    <div style={{ maxWidth: "1024px", margin: "10rem auto" }} {...props}>
+      {props.children}
+    </div>
+  );
 }
 
 export default Container;
