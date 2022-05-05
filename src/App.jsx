643e7f10ca72df8af7a4c66ed76fa8da720a7a49
nevz <projectnevz@gmail.com>
2022-05-05 22:17:36 +0800

    Add PrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate
+} from "react-router-dom";
 import AuthProvider from "./context/AuthProvider";
+import useAuth from "./hooks/useAuth";
 
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+
+function PrivateRoute() {
+  const { currentUser } = useAuth();
+  return currentUser ? <Outlet /> : <Navigate to="/signin" />;
+}
 
 function App() {
   return (
     <Router basename="/">
       <AuthProvider>
         <Routes>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route index element={<Home />} />
+            <Route path=":userID" element={<Profile />} />
+          </Route>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
         </Routes>
