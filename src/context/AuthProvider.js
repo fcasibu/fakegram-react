280af13073db924d9c08a4f0966c79210648543c
nevz <projectnevz@gmail.com>
2022-05-06 20:45:10 +0800

    Fix typo

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import AuthContext from "./AuthContext";
-import auth from "../firebase";
+import { auth } from "../firebase";
 
 function AuthProvider({ children }) {
-  const [currentUser, setCurretUser] = useState();
+  const [currentUser, setCurrentUser] = useState();
 
   function signUp({ email, password }) {
     return auth.createUserWithEmailAndPassword(email, password);
@@ -15,11 +15,12 @@ function AuthProvider({ children }) {
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurretUser(user);
-    });
+      setCurrentUser(user);
+   });
 
+   console.log('wtf')
     return unsubscribe;
-  });
+  }, []);
 
   const value = {
     currentUser,
