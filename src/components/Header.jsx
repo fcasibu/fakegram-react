b67f0ac8c4d7d89a413238ff3f0ee257bdcb2ca6
nevz <projectnevz@gmail.com>
2022-05-06 20:42:36 +0800

    Add logic for adding posts
    Can be refactored and improved performance wise

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,51 @@ import React from "react";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import { db, storage } from "../firebase";
+import firebase from "firebase/compat/app";
+
+function usersCollection(id) {
+  return db.collection("users").doc(id);
+}
+
+function imageRef(id, img) {
+  return storage.ref(`/images/${id}/${img.name}`);
+}
+
+async function isImagePosted(uid, img) {
+  const getData = await usersCollection(uid).get();
+  return getData.data().posts.some(post => post.id === img.name);
+}
+
+function postImage(uid, img, caption) {
+  imageRef(uid, img)
+    .getDownloadURL()
+    .then(url => {
+      usersCollection(uid).set(
+        {
+          posts: firebase.firestore.FieldValue.arrayUnion({
+            caption,
+            image: url,
+            comments: [],
+            likes: 0,
+            id: img.name
+          })
+        },
+        { merge: true }
+      );
+    });
+}
 
 const Header = React.memo(function Header() {
   const { currentUser } = useAuth();
+  const { uid, photoURL, displayName } = currentUser;
+
+  async function addPost(event) {
+    let img = event.target.files[0];
+    if (await isImagePosted(uid, img)) return;
+    await imageRef(uid, img).put(img);
+    postImage(uid, img, "hello");
+  }
 
   return (
     <header className="header">
@@ -22,11 +64,14 @@ const Header = React.memo(function Header() {
             </Link>
           </li>
           <li>
-            <button type="button">+</button>
+            <label htmlFor="upload-file">
+              <p>+</p>
+              <input id="upload-file" type="file" onChange={addPost} />
+            </label>
           </li>
           <li>
-            <Link to={`/${currentUser.uid}`}>
-              <img src={currentUser.photoURL} alt={currentUser.displayName} />
+            <Link to={`/${uid}`}>
+              <img src={photoURL} alt={displayName} />
             </Link>
           </li>
         </ul>
