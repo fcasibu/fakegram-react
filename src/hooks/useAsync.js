1b376267416e9d75daf6cc5006562e96a5ac014f
nevz <projectnevz@gmail.com>
2022-05-06 18:03:54 +0800

    Add useAsync hook

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,41 @@
+import { useReducer, useCallback } from 'react';
+
+function asyncReducer(state, action) {
+  switch (action.type) {
+    case "pending": {
+      return { status: "pending", data: null, error: null };
+    }
+    case "resolved": {
+      return { status: "resolved", data: action.data, error: null };
+    }
+    case "rejected": {
+      return { status: "rejected", data: null, error: action.error };
+    }
+    default: {
+      throw new Error(`Unexpected action type: ${action.type}`);
+    }
+  }
+}
+
+function useAsync(initialState = {}) {
+  const [state, dispatch] = useReducer(asyncReducer, {
+    status: "pending",
+    data: null,
+    error: null,
+    ...initialState,
+  });
+
+  const runAsync = useCallback(promise => {
+    dispatch({type: "pending"})
+    promise.then((data) => {
+      dispatch({type: "resolved", data})
+    }).catch(error => {
+      dispatch({type: "rejected", error})
+    })
+  }, [dispatch])
+
+
+  return { ...state, runAsync };
+}
+
+export default useAsync;
