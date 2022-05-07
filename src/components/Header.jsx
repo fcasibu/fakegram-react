35c3d11cd301c290b3c7ef01fd78d5472d5be3b7
nevz <projectnevz@gmail.com>
2022-05-07 18:38:09 +0800

    Remove unnecessary lines

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,53 +2,18 @@ import React from "react";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import { db, storage, firebase } from "../firebase";
-
-function usersCollection(id) {
-  return db.collection("users").doc(id);
-}
-
-function imageRef(id, img) {
-  return storage.ref(`/images/${id}/${img.name}`);
-}
-
-async function isImagePosted(uid, img) {
-  const getData = await usersCollection(uid).get();
-  return getData.data().posts.some(post => post.id === img.name);
-}
-
-function postImage(uid, img, caption) {
-  imageRef(uid, img)
-    .getDownloadURL()
-    .then(url => {
-      usersCollection(uid).set(
-        {
-          posts: firebase.firestore.FieldValue.arrayUnion({
-            caption,
-            image: url,
-            comments: [],
-            likes: 0,
-            id: img.name
-          })
-        },
-        { merge: true }
-      );
-    });
-}
+import Modal from "./Modal";
+import useModal from "../hooks/useModal";
+import UploadModal from "./UploadModal";
 
 const Header = React.memo(function Header() {
   const { currentUser } = useAuth();
   const { uid, photoURL, displayName } = currentUser;
-
-  async function addPost(event) {
-    let img = event.target.files[0];
-    if (await isImagePosted(uid, img)) return;
-    await imageRef(uid, img).put(img);
-    postImage(uid, img, "hello");
-  }
+  const { isModalOpen, openModal } = useModal();
 
   return (
     <header className="header">
+      {isModalOpen && <Modal component={UploadModal} />}
       <div>
         <h2>Site Logo</h2>
       </div>
@@ -63,10 +28,9 @@ const Header = React.memo(function Header() {
             </Link>
           </li>
           <li>
-            <label htmlFor="upload-file">
+            <button onClick={() => openModal()}>
               <p>+</p>
-              <input id="upload-file" type="file" onChange={addPost} />
-            </label>
+            </button>
           </li>
           <li>
             <Link to={`/${uid}`}>
@@ -80,3 +44,7 @@ const Header = React.memo(function Header() {
 });
 
 export default Header;
+/*  <label htmlFor="upload-file">
+              <p>+</p>
+              <input id="upload-file" type="file" onChange={changeHandler} />
+            </label>*/
