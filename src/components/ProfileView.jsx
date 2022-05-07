f7a01c6f8e0a1e64136e9e4d063f0618a69bd906
nevz <projectnevz@gmail.com>
2022-05-08 00:35:17 +0800

    Add logic for follow/unfollow

--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { db } from "../firebase";
+import { db, firebase } from "../firebase";
 import { updateProfile } from "firebase/auth";
 import styles from "../styles/ProfileView.module.css";
 import Header from "./Header";
+import useAuth from "../hooks/useAuth";
 
-function ProfileView({ user }) {
+function getUserFromDB(id) {
+  return db.collection("users").doc(id);
+}
+
+function setFollowState(getID, setID, field, state = "arrayUnion") {
+  getUserFromDB(getID).set(
+    {
+      [field]: firebase.firestore.FieldValue[state](setID)
+    },
+    { merge: true }
+  );
+}
+
+const ProfileView = React.memo(function ProfileView({ user }) {
+  const { currentUser } = useAuth();
+
+  const isCurrentUser = user.uid === currentUser.uid;
+  const isFollowing = user.followers.includes(currentUser.uid);
+  console.log(currentUser.uid);
   function renderPosts() {
     return user.posts.map(post => {
       return (
@@ -14,6 +33,16 @@ function ProfileView({ user }) {
         </div>
       );
     });
+  }
+
+  function followUser() {
+    setFollowState(user.uid, currentUser.uid, "followers");
+    setFollowState(currentUser.uid, user.uid, "following");
+  }
+
+  function unfollowUser() {
+    setFollowState(user.uid, currentUser.uid, "followers", "arrayRemove");
+    setFollowState(currentUser.uid, user.uid, "following", "arrayRemove");
   }
 
   return (
@@ -26,6 +55,11 @@ function ProfileView({ user }) {
             <div className={styles["profile-info-top"]}>
               <h2>{user.displayName}</h2>
               <button>Edit Profile</button>
+              {isCurrentUser ? null : !isFollowing ? (
+                <button onClick={followUser}>Follow</button>
+              ) : (
+                <button onClick={unfollowUser}>Unfollow</button>
+              )}
             </div>
             <div>
               <ul className={styles["profile-count"]}>
@@ -34,11 +68,11 @@ function ProfileView({ user }) {
                   <p>posts</p>
                 </li>
                 <li>
-                  <h2>{user.followers}</h2>
+                  <h2>{user.followers.length}</h2>
                   <p>followers</p>
                 </li>
                 <li>
-                  <h2>{user.following}</h2>
+                  <h2>{user.following.length}</h2>
                   <p>following</p>
                 </li>
               </ul>
@@ -53,7 +87,7 @@ function ProfileView({ user }) {
       </div>
     </div>
   );
-}
+});
 
 export default ProfileView;
 
@@ -61,8 +95,9 @@ ProfileView.propTypes = {
   user: PropTypes.shape({
     photoURL: PropTypes.string,
     displayName: PropTypes.string,
+    uid: PropTypes.string,
     posts: PropTypes.arrayOf(PropTypes.object),
-    followers: PropTypes.number,
-    following: PropTypes.number
+    followers: PropTypes.arrayOf(PropTypes.string),
+    following: PropTypes.arrayOf(PropTypes.string)
   })
 };
