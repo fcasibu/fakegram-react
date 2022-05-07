913ccd9b0c3db2da4b49aece94d0d4d77401b6e9
nevz <projectnevz@gmail.com>
2022-05-07 18:36:32 +0800

    Add modal for posting

---
+++ src/components/UploadModal.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import useModal from "../hooks/useModal";
+import styles from "../styles/Modal.module.css";
+
+function UploadModal() {
+  const { addPost } = useAuth();
+  const { closeModal } = useModal();
+  const [file, setFile] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [caption, setCaption] = useState();
+
+  function changeHandler(event) {
+    setFile(event.target.files[0]);
+  }
+  async function sharePost() {
+    setLoading(true);
+    await addPost(file, caption);
+    setLoading(true);
+    closeModal();
+  }
+
+  return (
+    <div className={styles["upload-modal"]}>
+      <h2>Upload your photos here</h2>
+      <p>{file && file.name}</p>
+      <label htmlFor="upload-file">
+        <button>Select from computer</button>
+        <input id="upload-file" type="file" onChange={changeHandler} />
+      </label>
+      <div className={styles["caption"]}>
+        <textarea
+          placeholder="Your caption"
+          value={caption}
+          onChange={e => setCaption(e.target.value)}
+        ></textarea>
+      </div>
+      <div className={styles.share}>
+        <button disabled={file === ""} onClick={sharePost}>
+          {loading ? "Loading..." : "Share"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UploadModal;
