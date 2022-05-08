c1dc2e6e2e811de2e0f96a4744cee6a428690a01
nevz <projectnevz@gmail.com>
2022-05-08 20:52:32 +0800

    Add logic to render users you follow

--- src/components/Followings.jsx
+++ src/components/Followings.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import useAuth from "../hooks/useAuth";
 import styles from "../styles/MainView.module.css";
 
 function Followings({ users }) {
+  const { currentUser } = useAuth();
+  function filterUsers() {
+    return users.filter(user => user.uid === currentUser.uid);
+  }
+
   function renderUsers() {
-    return users.map(user => {
+    // refactor
+    const following = users.filter(user => {
+      return filterUsers()[0].following.includes(user.uid);
+    });
+    return following.map(user => {
       return (
         <Link to={`/${user.uid}`} key={user.uid}>
           <img src={user.photoURL} />
