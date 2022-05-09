8db3625f51714ef1aeaaa2d5a15c264246a92220
nevz <projectnevz@gmail.com>
2022-05-09 22:47:38 +0800

    Small refactor
    Will do a big one soon

--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -11,31 +11,71 @@ function getUserFromDB(id) {
   return db.collection("users").doc(id);
 }
 
-async function setFollowState(getID, index, data) {
-  const userData = await getUserFromDB(getID).get();
-  const comment = userData.data().posts;
-  comment[index].comments.push(data);
-  getUserFromDB(getID).update(
-    {
-      posts: comment
-    },
-    { merge: true }
-  );
+function setUserFromDB(user, action) {
+  getUserFromDB(user.posterId).update({
+    [user.mainField]: action
+  });
+}
+
+function removeData(user, action) {
+  return (action[user.index][user.subField] = action[user.index][
+    user.subField
+  ].filter(id => id !== user.uid));
+}
+
+function addData(user, action) {
+  return action[user.index][user.subField][user.method](user.data);
+}
+
+async function interact(user) {
+  const userData = await getUserFromDB(user.posterId).get();
+  const action = userData.data()[user.mainField];
+  user.method !== "filter" ? addData(user, action) : removeData(user, action);
+  setUserFromDB(user, action);
 }
 function UserPosts({ users }) {
   const { currentUser } = useAuth();
   const [formValue, setFormValue] = useState("");
 
+  function likePost(event) {
+    const likes = JSON.parse(event.target.dataset.likes);
+    if (likes.includes(currentUser.uid)) {
+      interact({
+        uid: currentUser.uid,
+        posterId: event.target.id,
+        index: event.target.dataset.index,
+        data: null,
+        mainField: "posts",
+        subField: "likes",
+        method: "filter"
+      });
+    } else {
+      interact({
+        posterId: event.target.id,
+        index: event.target.dataset.index,
+        data: currentUser.uid,
+        mainField: "posts",
+        subField: "likes",
+        method: "push"
+      });
+    }
+  }
+
   function submitHandler(event) {
+    event.preventDefault();
     const data = {
       displayName: currentUser.displayName,
       uid: currentUser.uid,
       comment: formValue
     };
-    const uid = event.target.id;
-    const { index } = event.target.dataset;
-    event.preventDefault();
-    setFollowState(uid, index, data);
+    interact({
+      posterId: event.target.id,
+      index: +event.target.dataset.index,
+      data,
+      mainField: "posts",
+      subField: "comments",
+      method: "push"
+    });
     setFormValue("");
   }
 
@@ -52,15 +92,25 @@ function UserPosts({ users }) {
       return user.posts.map((post, index) => {
         return (
           <div className={styles["post"]} key={index}>
-            <div className={styles["post-top"]}>
+            <Link to={`/${user.uid}`} className={styles["post-top"]}>
               <img src={user.photoURL} />
               <h3>{user.displayName}</h3>
-            </div>
+            </Link>
             <img src={post.image} />
             <div className={styles["post-details"]}>
               <div className={styles["post-icons"]}>
                 <div>
-                  <FiHeart />
+                  <div
+                    data-likes={JSON.stringify(post.likes)}
+                    data-index={index}
+                    id={user.uid}
+                    onClick={likePost}
+                    className={
+                      post.likes.includes(currentUser.uid) ? styles.liked : ""
+                    }
+                  >
+                    <FiHeart style={{ pointerEvents: "none" }} />
+                  </div>
                   <FiMessageCircle />
                 </div>
                 <FiBookmark />
@@ -85,6 +135,7 @@ function UserPosts({ users }) {
                     onChange={e => setFormValue(e.target.value)}
                     placeholder="Add a comment"
                     minLength="4"
+                    maxLength="60"
                     required
                   />
                   <button type="submit">Post</button>
