ee5810fed7b3462b1e62ca28424b78eeb8ebaa3d
nevz <projectnevz@gmail.com>
2022-05-09 22:48:04 +0800

    Remove unnecessary lines

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import Followings from "./Followings";
 import styles from "../styles/MainView.module.css";
 import Suggestions from "./Suggestions";
-import useAuth from "../hooks/useAuth";
 import UserPosts from "./UserPosts";
 
 function MainView({ users }) {
-  const { followUser } = useAuth();
-
   return (
     <main className={styles.main}>
       <div className={styles["left-section"]}>
@@ -17,7 +13,7 @@ function MainView({ users }) {
         <UserPosts users={users} />
       </div>
       <div className={styles["right-section"]}>
-        <Suggestions followUser={followUser} users={users} />
+        <Suggestions users={users} />
       </div>
     </main>
   );
