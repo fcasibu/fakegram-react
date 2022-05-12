ddd3e968850cb7837eb5d9dff95ed7075f03893c
nevz <projectnevz@gmail.com>
2022-05-12 15:04:07 +0800

    Add ability to search users

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiHome, FiSearch, FiHeart, FiMessageCircle } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { db } from "../firebase";
 import useAuth from "../hooks/useAuth";
 import Modal from "./Modal";
 import useModal from "../hooks/useModal";
 import UploadModal from "./UploadModal";
+import SearchMenu from "./SearchMenu";
 
 function Header() {
   const { currentUser } = useAuth();
   const { uid, photoURL, displayName } = currentUser;
+  const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
   const { isModalOpen, openModal, closeModal } = useModal();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await db.collection("users").orderBy("createdAt").get();
+
+      data.docs.forEach(doc => {
+        setUsers(state => [...state, doc.data()]);
+      });
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setFilteredUsers(
+      users.filter(user => {
+        return user.displayName.toLowerCase().includes(query.toLowerCase());
+      })
+    );
+  }, [query]);
+
   return (
     <header className="header">
       {isModalOpen && (
@@ -27,7 +52,16 @@ function Header() {
         <div className="search-icon">
           <FiSearch />
         </div>
-        <input />
+        <input
+          onBlur={() => {
+            setTimeout(() => {
+              setQuery("");
+            }, 100);
+          }}
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        {!query.length ? null : <SearchMenu users={filteredUsers} />}
       </div>
       <nav>
         <ul>
