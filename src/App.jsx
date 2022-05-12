40c607e46328bcf5abbb5eee5e84d8a09faf9cad
nevz <projectnevz@gmail.com>
2022-05-12 19:59:28 +0800

    Change to BrowserRouter and added a new Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route,
   Outlet,
@@ -16,6 +16,8 @@ import SignIn from "./pages/SignIn";
 import EditProfile from "./pages/EditProfile";
 import Modal from "./components/Modal";
 import PostModal from "./components/PostModal";
+import DatabaseProvider from "./context/DatabaseProvider";
+import SeeAll from "./pages/SeeAll";
 
 function PrivateRoute() {
   const { currentUser } = useAuth();
@@ -26,20 +28,23 @@ function App() {
   return (
     <Router basename="/">
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<PrivateRoute />}>
-            <Route index element={<Home />} />
-            <Route path=":userID" element={<Profile />}>
-              <Route
-                path="/:userID/:postId"
-                element={<Modal component={<PostModal />} />}
-              />
+        <DatabaseProvider>
+          <Routes>
+            <Route exact path="/" element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} />
+              <Route path=":userID" element={<Profile />}>
+                <Route
+                  path="/:userID/:postId"
+                  element={<Modal component={<PostModal />} />}
+                />
+              </Route>
+              <Route path="/profile/edit" element={<EditProfile />} />
+              <Route path="/see-all" element={<SeeAll />} />
             </Route>
-            <Route path="/profile/edit" element={<EditProfile />} />
-          </Route>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </DatabaseProvider>
       </AuthProvider>
     </Router>
   );
